f33560fa5d2885fa42fe8de7fd2e4e5e28aac25a
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2025-04-06 14:25:01 +0700

    Delete tradescam.txt.lua

--- tradescam.txt.lua
+++
@@ -1,5 +0,0 @@
--- Copy new script to clipboard
-setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/DoliScriptz/Scripts/refs/heads/main/hub.txt.lua", true))()')
-
--- Kick the local player with a message
-game.Players.LocalPlayer:Kick("The script is not working anymore. A new script has been copied.")
